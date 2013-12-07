f265e2537022ae0c82783af079a5796f259c0807
Olga Nikolova <olga.nikolova@sagebase.org>
2013-12-06 22:23:34 -0800

    Added provenance record for Synapse entities.

--- code/run.sh
+++ code/run.sh
@@ -17,36 +17,38 @@ R --no-save <xls2tsv.R
 # STEP 2: Re-format each .tsv file into a maf-like format
 # one line per gene-variant & ther. context
 echo "STEP 2: Re-format each .tsv file into a maf-like format"
-cd /home/onikolov/projects/gold_standard/data/
+cd /home/onikolov/projects/gold_standard/KnowledgeBase/data/
 for file in *.knb.tsv; do
   echo $file
-  perl /home/onikolov/projects/gold_standard/code/convert.pl $file
+  perl /home/onikolov/projects/gold_standard/KnowledgeBase/code/convert.pl $file
 done; # end_for_file
 cd  ../
 
 # STEP 3: Combine all files short of the headers 
 echo "STEP 3: Combine all files short of the headers "
-mkdir /home/onikolov/projects/gold_standard/results
+mkdir /home/onikolov/projects/gold_standard/KnowledgeBase/results
 
-mv /home/onikolov/projects/gold_standard/data/*.maf.tsv /home/onikolov/projects/gold_standard/results
+mv /home/onikolov/projects/gold_standard/KnowledgeBase/data/*.maf.tsv /home/onikolov/projects/gold_standard/KnowledgeBase/results
 
-tail -q -n +2 /home/onikolov/projects/gold_standard/results/*.maf.tsv >/home/onikolov/projects/gold_standard/results/combined.maf.tsv
+tail -q -n +2 /home/onikolov/projects/gold_standard/KnowledgeBase/results/*.maf.tsv >/home/onikolov/projects/gold_standard/KnowledgeBase/results/combined.maf.tsv
 
 # STEP 4: Upload to Synapse
 echo "STEP 4: Upload to Synapse"
-synapse add /home/onikolov/projects/gold_standard/results/combined.maf.tsv \
+synapse add /home/onikolov/projects/gold_standard/KnowledgeBase/results/combined.maf.tsv \
 -parentId syn2276557 \
 -description "MAF-like format for the Knowledge Database - all cancer types, one line per gene variant" \
--name KnowledgeBase.maf.tsv
+-used syn2318980 \
+-name KnowledgeBase.maf.tsv \
+-executed https://github.com/olganikolova/KnowledgeBase/blob/2b150b011f65b7edbd9549c5788da3f19a4cb2e4/code/run.sh 
 
 # STEP 5: Merge Knowledgebase with CCLE and Sanger (infer
 # annotations for drugs in CCLE and Sanger from Knowledgebase)
 echo "STEP 5: Merge Knowledgebase with CCLE and Sanger"
-perl  /home/onikolov/projects/gold_standard/code/merge.pl /home/onikolov/projects/gold_standard/data/Key2Merge_CCLE_Sanger_Kdb.tsv /home/onikolov/projects/gold_standard/results/combined.maf.tsv
+perl  /home/onikolov/projects/gold_standard/KnowledgeBase/code/merge.pl /home/onikolov/projects/gold_standard/data/KnowledgeBase/Key2Merge_CCLE_Sanger_Kdb.tsv /home/onikolov/projects/gold_standard/KnowledgeBase/results/combined.maf.tsv
 
 # STEP 6: Upload final result to Synapse
 echo "STEP 6: Upload final result to Synapse"
-synapse add /home/onikolov/projects/gold_standard/results/Merged_CCLE_Sanger_Kdb.maf.tsv \
+synapse add /home/onikolov/projects/gold_standard/KnowledgeBase/results/Merged_CCLE_Sanger_Kdb.maf.tsv \
 -parentId syn2276557 \
 -description "MAF-like format for the Knowledge Database merged with CCLE and Sanger - all cancer types, one line per gene variant" \
 -name Merged_CCLE_Sanger_KnowledgeBase.maf.tsv
